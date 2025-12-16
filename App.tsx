699555f0f41c6c90d6c9105a61c0c97e6ac1c564
Tommy <tommymoleong@gmail.com>
2025-12-17 01:57:20 +0800

    Append redux + redux thunk middleware

--- App.tsx
+++ App.tsx
@@ -2,34 +2,38 @@ import React from 'react';
 import { Image, StatusBar, useColorScheme, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
 import BottomTabNavigator from './src/navigator/BottomTabNavigator';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <NavigationContainer>
-          <View
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              width: '100%',
-              backgroundColor: 'white'
-            }}
-          >
-            <Image
-              source={require('./src/assets/icons/tmbd-icon.png')}
-              style={{ width: 80, height: 80, marginTop: 5, marginBottom: 5 }}
-            />
-          </View>
-          <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-          <BottomTabNavigator />
-        </NavigationContainer>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1 }}>
+          <NavigationContainer>
+            <View
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100%',
+                backgroundColor: 'white'
+              }}
+            >
+              <Image
+                source={require('./src/assets/icons/tmbd-icon.png')}
+                style={{ width: 80, height: 80, marginTop: 5, marginBottom: 5 }}
+              />
+            </View>
+            <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+            <BottomTabNavigator />
+          </NavigationContainer>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </Provider>
   );
 }
 
